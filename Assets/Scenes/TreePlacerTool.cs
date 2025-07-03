1106a66887b1e90f5a4f7ab7b66ea8ae9fe866ea
shaked peller <shaked.peller@gmail.com>
2025-07-03 14:15:09 +0300

    add buttons for tree and flower

--- Assets/Scenes/TreePlacerTool.cs
+++ Assets/Scenes/TreePlacerTool.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimplePlacer : MonoBehaviour
 {
-    public GameObject prefabToPlace;
+    public GameObject treePrefab;
+    public GameObject flowerPrefab;
+    private GameObject prefabToPlace;
+
+    public Button treeButton;
+    public Button flowerButton;
+
+    void Start()
+    {
+        // ברירת מחדל - עץ
+        prefabToPlace = treePrefab;
+
+        // מאזינים לכפתורים
+        treeButton.onClick.AddListener(() => prefabToPlace = treePrefab);
+        flowerButton.onClick.AddListener(() => prefabToPlace = flowerPrefab);
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && prefabToPlace != null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, 100f))
             {
-                 Vector3 spawnPosition = hit.point + Vector3.up * (prefabToPlace.transform.localScale.y / 2);
+                Vector3 spawnPosition = hit.point + Vector3.up * (prefabToPlace.transform.localScale.y / 2);
                 Instantiate(prefabToPlace, spawnPosition, Quaternion.identity);
                 Debug.Log("Placed prefab at: " + hit.point);
             }
